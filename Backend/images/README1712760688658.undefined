9b09fcf329981c57ade3ff8306102a8937d41052
Fabien Gosse <f.gosse77@gmail.com>
2024-04-18 11:23:55 +0200

    Add Backend, remove console.log

---
+++ Backend/images/README1712760688658.undefined
@@ -0,0 +1,1 @@
+# Front-Sophie-Bluel
---
+++ FrontEnd/assets/js/routes.js
@@ -0,0 +1,5 @@
+export default {
+    API_ROUTE_WORKS: "http://localhost:5678/api/works",
+    API_ROUTE_CATEGORIES: "http://localhost:5678/api/categories",
+    API_ROUTE_LOGIN: "http://localhost:5678/api/users/login",
+};
